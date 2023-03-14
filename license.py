aa3585cd3749aa896f9139b94d6cb431ba41517b
jluevan13 <luevano.jesus13@gmail.com>
2023-03-14 11:48:47 -0700

    get usage by account group

--- license.py
+++ license.py
@@ -2,7 +2,11 @@ import requests
 import json
 import os
 
-url = "https://api4.prismacloud.io/login"
+
+### Create Auth Token for tenant
+url = (
+    "https://api4.prismacloud.io/login"  # update url based on tenant (i.e, app3, app2)
+)
 
 payload = {}
 payload["username"] = os.environ.get("prismaUserName")
@@ -17,47 +21,47 @@ auth_token = requests.request("POST", url, headers=headers, data=payload_json)
 auth_token_data = auth_token.json()
 token = auth_token_data["token"]
 
-# print(auth_token.text)
-
-
+### Get Group Ids
 url = "https://api4.prismacloud.io/cloud/group"
 
 group_payload = {}
 group_payload["accountIds"] = []
 headers = {"Accept": "application/json; charset=UTF-8", "x-redlock-auth": token}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+account_groups = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
 
-# url = "https://api4.prismacloud.io/license/api/v2/time_series"
-url = "https://api4.prismacloud.io/license/api/v2/usage"
+def get_credit_usage(group_id, time, unit):
+    url = "https://api4.prismacloud.io/license/api/v2/usage"
 
-usage_payload = {}
-usage_payload["accountIds"] = []
-usage_payload["accountGroupIds"] = ["1a687eb3-6d92-4142-8e5e-544b7ebc2c09"]
-usage_payload["cloudTypes"] = [
-    "aws",
-    "azure",
-    "oci",
-    "alibaba_cloud",
-    "gcp",
-    "others",
-    "repositories",
-]
-usage_payload["timeRange"] = {
-    "type": "relative",
-    "value": {"amount": "3", "unit": "month"},
-}
-usage_payload_json = json.dumps(usage_payload)
-# print(group_payload_json)
+    usage_payload = {}
+    usage_payload["accountIds"] = []
+    usage_payload["accountGroupIds"] = [group_id]
+    usage_payload["cloudTypes"] = [
+        "aws",
+        "azure",
+        "oci",
+        "alibaba_cloud",
+        "gcp",
+        "others",
+        "repositories",
+    ]
+    usage_payload["timeRange"] = {
+        "type": "relative",
+        "value": {"amount": time, "unit": unit},
+    }
+    usage_payload_json = json.dumps(usage_payload)
 
-headers = {
-    "Content-Type": "application/json; charset=UTF-8",
-    "Accept": "application/json; charset=UTF-8",
-    "x-redlock-auth": token,
-}
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Accept": "application/json; charset=UTF-8",
+        "x-redlock-auth": token,
+    }
+    response = requests.request("POST", url, headers=headers, data=usage_payload_json)
 
-response = requests.request("POST", url, headers=headers, data=usage_payload_json)
+    return response.json()
 
-# print(response.json())
+
+for group in account_groups.json():
+    usage_by_group = get_credit_usage(group["id"], "1", "month")
+    print(f'{group["id"]}: {usage_by_group["stats"]["total"]}')
